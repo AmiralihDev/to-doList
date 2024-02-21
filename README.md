64f3e5c5533811c0ef1eb889adc396ac06ecda73
AmiralihDev <ophmmh.uib@gmail.com>
2024-02-21 17:34:38 +0330

    feat : if  key down to enter your task name is  save

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 **E-mail : ophmmh.uib@gmail.com**,
 **date : 2024/2/21**
 # Project information 
+### What is To Do List? 
+
+<P>The meaning is very simple. To Do List is a list of tasks and activities that must be done.</P>
+
 this project is home-work 
 in this project you can add task and delete that and that task is save in local storage and if you refresh the site your task is remise 
 if you write task name whit html code and click on add button, the task is not sucssesful and task input is empty 
--- script.js
+++ script.js
@@ -1,59 +1,57 @@
-let userInput = document.querySelector('#userInput')
-let add = document.querySelector('#push')
-add.addEventListener("click" ,  () => {
-    if(userInput.value == ""){
-        alert("Please Enter a Task")
+
+let addBtn = document.querySelector("#push");
+const userInput = document.querySelector("#userInput");
+const tasks = document.querySelector("#tasks");
+addBtn.addEventListener("click",add)
+function add() {
+  if (userInput.value === "") {
+    alert("please enter any task!");
+  } else {
+   
+    let taskName = document.createElement("li");
+    taskName.innerText = userInput.value;
+    taskName.className = "taskname"
+    let trash = document.createElement("span");
+    trash.innerText = "Delete";
+    trash.className = "trash"
+    let btnEdit = document.createElement("button")
+    btnEdit.className = "edit"
+    btnEdit.innerText =  "Edit"
+    taskName.appendChild(btnEdit)
+    taskName.appendChild(trash);
+    tasks.appendChild(taskName);
+  }
+  userInput.value = "";
+  save();
+}
+tasks.addEventListener("click",
+   (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      save();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      save();
     }
-    else{
-        if (userInput.value.includes('<')) {
-            
-        }
-        else{
-
-            document.querySelector('#tasks').innerHTML += `
-                <div class="task">
-                    <span id="taskname">
-                        ${userInput.value}
-                    </span>
-                    <button class="delete">
-                        <i class="far fa-trash-alt"></i>
-                    </button>
-                    
-                </div>
-            `;
-    
-            userInput.value = "";
-            save()
-            let current_tasks = document.querySelectorAll(".delete");
-    
-    
-    
-            for(let i=0; i<current_tasks.length; i++){
-                current_tasks[i].onclick = function(){
-                    this.parentNode.remove();
-                    save()
-                }
-            }
-            
-            
-            
-            let tasks = document.querySelectorAll(".task");
-            for(let i=0; i<tasks.length; i++){
-                tasks[i].onclick = function(){
-                    this.classList.toggle('completed');
-                }
-            }
-            userInput.value = "";
-        }
-
+     else if (e.target.tagName === "BUTTON") {
+      e.target.parentElement.remove();
+      userInput.value = 
+      save();
     }
+  },
+  
+);
+function save() {
+  localStorage.setItem("data", tasks.innerHTML);
+}
+function show() {
+  tasks.innerHTML = localStorage.getItem("data");
+}
 
-}
-)
-function save(){
-    localStorage.setItem("data", tasks.innerHTML)
-}
-function show(){
-    tasks.innerHTML = localStorage.getItem("data")
-}
-show()
+// Add event listener for "Enter" key press
+userInput.addEventListener("keyup",  (event) => {
+  if (event.key === "Enter") {
+    add();
+  }
+});
+show();
